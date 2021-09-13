2d935f28953e90332c9abf4186556372a567b3c0
GabrielCF10 <gabrielcarvalhoc100@gmail.com>
2021-09-13 13:05:40 -0300

    Mudanças na tela de parabadminton

--- lib/pages/parabadminton.dart
+++ lib/pages/parabadminton.dart
@@ -11,98 +11,99 @@ class ParaBadminton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      padding:EdgeInsets.only(left: 10.0, right: 10.0, bottom: 40.0), 
+      padding: EdgeInsets.only(left: 10.0, right: 10.0, bottom: 20.0),
       child: Stack(
-      children: <Widget>[
-        Padding(
-          padding: EdgeInsets.all(7.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                height: 70.0,
-                child: RaisedButton(
-                  child: Text("Equipamentos",
-                      style: TextStyle(fontSize: 20.0, color: Colors.black)),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0),
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(7.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  height: 70.0,
+                  child: RaisedButton(
+                    child: Text("Equipamentos",
+                        style: TextStyle(fontSize: 20.0, color: Colors.black)),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    color: Color.fromARGB(255, 203, 236, 241),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  EquipamentosPara()));
+                    },
                   ),
-                  color: Color.fromARGB(255, 203, 236, 241),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (BuildContext context) =>
-                                EquipamentosPara()));
-                  },
                 ),
-              ),
-              SizedBox(
-                height: 75.0,
-              ),
-              Container(
-                height: 70.0,
-                child: RaisedButton(
-                  child: Text("Regras do Badminton",
-                      style: TextStyle(fontSize: 20.0, color: Colors.black)),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0),
+                SizedBox(
+                  height: 75.0,
+                ),
+                Container(
+                  height: 70.0,
+                  child: RaisedButton(
+                    child: Text("Regras Parabadminton",
+                        style: TextStyle(fontSize: 20.0, color: Colors.black)),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    color: Color.fromARGB(255, 203, 236, 241),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) => RegrasPara()));
+                    },
                   ),
-                  color: Color.fromARGB(255, 203, 236, 241),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (BuildContext context) => RegrasPara()));
-                  },
                 ),
-              ),
-              SizedBox(
-                height: 75.0,
-              ),
-              Container(
-                height: 70.0,
-                child: RaisedButton(
-                  child: Text("História",
-                      style: TextStyle(fontSize: 20.0, color: Colors.black)),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0),
+                SizedBox(
+                  height: 75.0,
+                ),
+                Container(
+                  height: 70.0,
+                  child: RaisedButton(
+                    child: Text("História",
+                        style: TextStyle(fontSize: 20.0, color: Colors.black)),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    color: Color.fromARGB(255, 203, 236, 241),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  HistoriaPara()));
+                    },
                   ),
-                  color: Color.fromARGB(255, 203, 236, 241),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (BuildContext context) => HistoriaPara()));
-                  },
                 ),
-              ),
-              SizedBox(
-                height: 75.0,
-              ),
-              Container(
-                height: 70.0,
-                child: RaisedButton(
-                  child: Text("Características do Esporte",
-                      style: TextStyle(fontSize: 20.0, color: Colors.black)),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0),
+                SizedBox(
+                  height: 75.0,
+                ),
+                Container(
+                  height: 70.0,
+                  child: RaisedButton(
+                    child: Text("Características do Esporte",
+                        style: TextStyle(fontSize: 20.0, color: Colors.black)),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    color: Color.fromARGB(255, 203, 236, 241),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) => Carac()));
+                    },
                   ),
-                  color: Color.fromARGB(255, 203, 236, 241),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (BuildContext context) => Carac()));
-                  },
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-      ],
-    ),
+        ],
+      ),
     );
   }
 }
